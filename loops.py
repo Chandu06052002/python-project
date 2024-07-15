b78d5bac88a1c85596ae5476c11258dac3af0330
Chandu <sabbisettigopichandu@gmail.com>
2024-07-15 18:54:48 +0530

    loops program modified

--- loops.py
+++ loops.py
@@ -6,3 +6,21 @@ for i in range(1,11):
 fruits = ['banana','apple','grapes','pineapple']
 for fruit in fruits:
     print(fruit) 
+
+
+# WHILE LOOP
+
+i = 1
+while i <= 5:
+    print(i)
+    i+=1
+
+
+# WHILE LOOP INSIDE THE FOR LOOP
+
+for num in range(1,6):
+    num2 = 1
+    while num2 <= 5:
+        print(num2)
+        num2 +=1
+    print(num)
