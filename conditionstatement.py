d031485de6c80f44fbf29287742e3f54a4a96615
Chandu <sabbisettigopichandu@gmail.com>
2024-07-15 19:48:51 +0530

    statements program

---
+++ conditionstatement.py
@@ -0,0 +1,36 @@
+# if condition
+
+a = 5
+
+if a <= 10 :
+    print("a is less than or equal to 10")
+
+# if-else condition
+
+b = 5
+
+if b >= 10:
+    print("b is less than 5")
+else:
+    print("b s greater than 5")
+
+# if elif condition
+
+day = 6
+
+if day == 1:
+    print("monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("wednesday")
+elif day == 4:
+    print("thursday")
+elif day == 5:
+    print("friday")
+elif day == 6:
+    print("saturday")
+elif day == 7:
+    print("sunday")
+else:
+    print("no day is available")
