f2dfc4fd4270446767889867a9786919a04a2709
Chandu <sabbisettigopichandu@gmail.com>
2024-07-15 15:56:40 +0530

    operators program

--- identifiers.py
+++
@@ -1,4 +0,0 @@
-print("there are different types of identifiers")
-
-
-print("we can modify git hub code")
---
+++ operators.py
@@ -0,0 +1,27 @@
+a = 10
+b = 5
+
+#ARTHEMATIC OPERATORS 
+
+print("addition of a and b is ", a + b)
+print("subtraction of a and b is ", a - b)
+print("multiplication of a and b is ", a * b)
+print("division of a and b is ", a / b)
+print("modules of a and b is ", a % b)
+print("power of a and b is ", a ** 2)
+
+#COMPARISION OPERATORS
+
+print("a is greater than b :",a > b)
+print("b is lesser than a :",b < a)
+print("a is not equal to b :",a != b)
+print("a is euqal to b :", a == b)
+print("a is greater than or equal to 10 :", a >= 10)
+print("b is lesser than or equal to 5 :", b <= 5)
+
+
+# LOGICAL OPERATORS
+
+print("and operator :", a == 10 and b == 5)
+print("or operator :",a ==5 or b == 5)
+print("not operator :",not(a == 10 and b == 5))
